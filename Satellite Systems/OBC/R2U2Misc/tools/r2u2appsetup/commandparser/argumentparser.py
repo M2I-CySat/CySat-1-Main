7acd2abdfb26dc28eed45a4ddafaf9dcb9def869
Henry Shires <hcsgeek@gmail.com>
2022-10-04 21:22:53 -0500

    Remove discoboard and R2U2misc
    Remove discoboard and R2U2misc

--- Satellite Systems/OBC/R2U2Misc/tools/r2u2appsetup/commandparser/argumentparser.py
+++
@@ -1,4 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description='accept and launch subcommands')
-parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-parser.parse_args()
